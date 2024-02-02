13299040104f8df6028d6813727545c5fbbe1e08
TonimatasDEV <amatascubero@gmail.com>
2024-02-02 20:21:31 +0100

    Finish gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,22 @@
+import java.net.URI
+
 plugins {
     java
+    `maven-publish`
+    signing
 }
 
-group = "dev.tonimatas"
-version = "1.0-SNAPSHOT"
+apply(plugin = "maven-publish")
+apply(plugin = "signing")
+
+java {
+    toolchain { languageVersion.set(JavaLanguageVersion.of(8)) }
+    withJavadocJar()
+    withSourcesJar()
+}
+
+group = "dev.tonimatas.packetlib"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -11,4 +24,74 @@ repositories {
 
 dependencies {
     
+}
+
+tasks.compileJava {
+    options.encoding = "UTF-8"
+    options.javaModuleVersion.set(provider { project.version as String })
+}
+
+tasks.withType<Javadoc> {
+    options.encoding = "UTF-8"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = rootProject.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            from(components["java"])
+
+            pom {
+                withXml {
+                    val root = asNode()
+                    root.appendNode("description", project.description)
+                    root.appendNode("name", project.name)
+                    root.appendNode("url", "https://github.com/TonimatasDEV/PacketLib.git")
+                }
+
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("TonimatasDEV")
+                    }
+                }
+
+                scm {
+                    url.set("https://github.com/TonimatasDEV/PacketLib.git")
+                }
+            }
+        }
+    }
+
+    repositories {
+        // Sonatype OSSRH
+        maven {
+            url = URI("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = System.getenv("USERNAME")
+                password = System.getenv("PASSWORD")
+            }
+        }
+    }
+}
+
+signing {
+    isRequired = gradle.taskGraph.hasTask("publish")
+    sign(publishing.publications["maven"])
+}
+
+tasks.withType<PublishToMavenRepository>() {
+    dependsOn(tasks.publishToMavenLocal)
+}
+
+tasks.withType<Jar>() {
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
 }
